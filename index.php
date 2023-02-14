a4e8f2881ed9b3b542d501968e24e84ea16b6e58
pedro.barrio <pedro.barrio@netexcloud.net>
2023-02-14 13:19:37 +0100

    code-test-back add geolocation

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Prueba de código para MarketGoo. ¡Lee el README.md!
  */
-require __DIR__."/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use GraphQL\Error\Debug;
 use GraphQL\Error\FormattedError;
@@ -13,6 +13,21 @@ use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Schema;
 
+function getIpRegionByIp(string $ip): string
+{
+    $baseUrl = "http://api.ipstack.com/"; // free enroll. free use. https://ipwhois.io/documentation can be use instead
+    //https://rapidapi.com/whoisapi/api/ip-geolocation/ => free and low latence
+    $fullUrl = sprintf($baseUrl . "%s?access_key=23dc9996807fa1f7d61a2b5acf791a23", $ip);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $fullUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $data = json_decode($result, true);
+    return "{$data['city']} - {$data['region_name']} ({$data['country_name']}) ";
+}
+
 // Datos estáticos que modelan los resultados de la consulta GraphQL
 $users = [
     1 => ["id" => 1, "name" => "Sergio Palma", "ip" => "188.223.227.125"],
@@ -20,13 +35,14 @@ $users = [
     3 => ["id" => 3, "name" => "Fito Cabrales", "ip" => "77.162.109.160"]
 ];
 
-// Definimos el schema del tipo de dato "Usuario" para GraphQL
+// Definimos el schema del tipo de dato "Usuario" para GraphQL - Hacer el Usuario como schema
 $graphql_user_type = new ObjectType([
     "name" => "User",
     "fields" => [
         "id" => Type::int(),
         "name" => Type::string(),
-        "ip" => Type::string()
+        "ip" => Type::string(),
+        "ip_region" => Type::string()
     ]
 ]);
 
@@ -34,7 +50,7 @@ $graphql_user_type = new ObjectType([
 // la ruta "/graphql" para este test. Todo lo demás es por defecto.
 $app = new Slim\App();
 
-$app->map(["GET", "POST"], "/graphql", function(Request $request, Response $response) {
+$app->map(["GET", "POST"], "/graphql", function (Request $request, Response $response) {
     global $users, $graphql_user_type;
     $debug = Debug::INCLUDE_DEBUG_MESSAGE | Debug::INCLUDE_TRACE;
     try {
@@ -49,12 +65,20 @@ $app->map(["GET", "POST"], "/graphql", function(Request $request, Response $resp
                                 "id" => Type::nonNull(Type::int())
                             ],
                             "resolve" => function ($rootValue, $args) use ($users) {
+                                $users[intval($args["id"])]["ip_region"] =
+                                    getIpRegionByIp(
+                                        $users[intval($args["id"])]["ip"]
+                                    );
                                 return $users[intval($args["id"])] ?? null;
                             }
                         ],
                         "users" => [
                             "type" => Type::listOf($graphql_user_type),
-                            "resolve" => function() use ($users) {
+                            "resolve" => function () use ($users) {
+                                foreach ($users as &$user) {
+                                    $user["ip_region"] = getIpRegionByIp($user["ip"]);
+                                }
+
                                 return $users;
                             }
                         ]
